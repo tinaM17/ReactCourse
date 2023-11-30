dbd5426715349ddedcc36c03a78097da55e42055
Tina Majumder <tinamajumder@Tinas-Air.lan>
2023-11-30 21:11:42 +0530

    logic of useState batch operation

--- 02_ReactCounter/src/App.jsx
+++ 02_ReactCounter/src/App.jsx
@@ -10,7 +10,14 @@ function App() {
     if(count>=20){
       return 
     }
-    setCount(count+1)
+    // setCount(count+1)
+    // setCount(count+1)
+    //This will not increase the counter by 2 because useState take this operation as a batch operation and when it sees that two operations are similar then it modifies only one
+    // So when we needs to change two modification how we can achieve this?
+    // using setCounter(callback fn)
+    setCount(prevCounter=>prevCounter+1)
+    setCount((prevCounter) => prevCounter + 1);
+    // Now this will update counter by 2. 
   }
   const decreaseCounter = () => {
     if (count <= 0) {
